7231c4b359ec7d38f446fc3cd051b4c73f299a84
Jakub Woźniak <jakubwozniak0611@gmail.com>
2022-10-19 19:53:01 +0200

    Zadanie Lab02

--- Lab01/zad1.py
+++ Lab01/zad1.py
@@ -1,15 +1,29 @@
 import sys
+import os
 import paramiko
 
 def connect_SSH():
     ssh = paramiko.SSHClient()
     
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('sigma.ug.edu.pl',username='jwozniak',key_filename='/home/jwozniak/PSW/Lab01/SSH')
+    ssh.connect('sigma.ug.edu.pl',username='jwozniak',key_filename='C:/Users/jakub/.ssh/id_rsa')
 
     stdin, stdout, stderr = ssh.exec_command('who -H')
     outlines = stdout.readlines()
     resp=''.join(outlines)
+    print('Zalogowani: ')
     print(resp)
+
+    url = input("Podaj sciezke >> ")
+
+    stdin, stdout, stderr = ssh.exec_command(f'test -f {url} && echo "y"')
+    lines = stdout.readlines()
+
+    if(lines==['y\n']):
+        stdin, stdout, stderr = ssh.exec_command(f'cat {url}')
+        print("Zawartosc pliku: " + str(stdout.readlines()))
+    else:
+        print("Nie istnieje")    
+        print(stdout.readlines())
 
 connect_SSH()
